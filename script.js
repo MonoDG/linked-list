b340079b2fba37089e49021bb212fd74743b82c3
MonoDG <monodg@proton.me>
2024-01-02 13:47:00 -0500

    Add contains, find and toString, some refactoring

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ class LinkedList {
 
     constructor() {
         this.#_nbNodes = 0;
-        this.#_head = new Node();
-        this.#_tail = this.#_head;
+        this.#_head = null;
+        this.#_tail = null;
     }
 
-    // Adds a new node containing value to the end of the list
     append(value) {
-        if (this.#_head.value === null) {
-            this.#_head.value = value;
+        if (this.#_head === null) {
+            this.#_head = new Node(value);
+            this.#_tail = this.#_head;
         } else {
             this.#_tail.nextNode = new Node(value);
             this.#_tail = this.#_tail.nextNode;
@@ -20,10 +20,10 @@ class LinkedList {
         this.#_nbNodes++;
     }
 
-    // Adds a new node containing value to the start of the list
     prepend(value) {
-        if (this.#_head.value === null) {
-            this.#_head.value = value;
+        if (this.#_head === null) {
+            this.#_head = new Node(value);
+            this.#_tail = this.#_head;
         } else {
             this.newHead = new Node(value);
             this.newHead.nextNode = this.#_head;
@@ -55,16 +55,47 @@ class LinkedList {
     }
 
     pop() {
-        if (this.#_nbNodes > 0) {
-            let oldTail = this.#_tail;
-            this.#_nbNodes--;
-            this.#_tail = this.at(this.#_nbNodes - 1);
-            this.#_tail.nextNode = null;
-            return oldTail;
+        if (this.#_tail === null) return null;
+        let poppedTail = this.#_tail;
+        this.#_nbNodes--;
+        this.#_tail = this.at(this.#_nbNodes - 1);
+        this.#_tail.nextNode = null;
+        if (this.#_nbNodes === 0) {
+            this.#_head = null;
+            this.#_tail = null;
         }
-        this.#_head = new Node();
-        this.#_tail = this.#_head;
+        return poppedTail;
+    }
+
+    contains(value) {
+        let currentNode = this.#_head;
+        while (currentNode !== null) {
+            if (currentNode.value === value) return true;
+            currentNode = currentNode.nextNode;
+        }
+        return false;
+    }
+
+    find(value) {
+        let currentNode = this.#_head;
+        let currentIndex = 0;
+        while (currentNode !== null) {
+            if (currentNode.value === value) return currentIndex;
+            currentNode = currentNode.nextNode;
+            currentIndex++;
+        }
         return null;
+    }
+
+    toString() {
+        let currentNode = this.#_head;
+        let output = "";
+        while (currentNode !== null) {
+            output += `(${currentNode.value}) -> `;
+            currentNode = currentNode.nextNode;
+        }
+        output += "null";
+        return output;
     }
 }
 
@@ -76,34 +107,60 @@ class Node {
 }
 
 let mylist = new LinkedList();
-[1, 2, 3, 4, 5, 6, 7].forEach((value) => mylist.append(value));
-console.log(mylist);
+console.log(mylist.toString());
+// [1, 2, 3, 4, 5, 6, 7].forEach((value) => mylist.append(value));
+// console.log(mylist.toString());
 
 mylist.prepend(8);
-mylist.prepend(9);
-mylist.append(10);
-mylist.append(15);
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-console.log(mylist.pop());
-let currentNode = mylist.head();
-while (currentNode !== null) {
-    console.log(currentNode.value);
-    currentNode = currentNode.nextNode;
-}
+mylist.prepend(10);
+mylist.append(3);
+console.log(mylist.toString());
+console.log(`Size: ${mylist.size()}`);
+console.log(`Head: ${mylist.head().value}`);
+console.log(`Tail: ${mylist.tail().value}`);
+console.log(`At 0: ${mylist.at(0).value}`);
+console.log(`At 1: ${mylist.at(1).value}`);
+console.log(`At 2: ${mylist.at(2).value}`);
+console.log(`Pop: ${mylist.pop().value}`);
+console.log(`Pop: ${mylist.pop().value}`);
+console.log(`Pop: ${mylist.pop().value}`);
+console.log(`Pop: ${mylist.pop()}`);
+console.log(mylist.toString());
+mylist.append(4);
+mylist.append(16);
+mylist.append(255);
+mylist.append(22);
+mylist.prepend(11);
+mylist.prepend(6);
+console.log(mylist.toString());
+console.log(`Contains 22? ${mylist.contains(22)}`);
+console.log(`Contains 100? ${mylist.contains(100)}`);
+console.log(`Finds 255 at index? ${mylist.find(255)}`);
+console.log(`Finds 100 at index? ${mylist.find(100)}`);
+// mylist.prepend(9);
+// mylist.append(10);
+// mylist.append(15);
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// console.log(mylist.pop());
+// let currentNode = mylist.head();
+// while (currentNode !== null) {
+//     console.log(currentNode.value);
+//     currentNode = currentNode.nextNode;
+// }
 
-console.log(mylist.size());
-console.log(mylist.head());
-console.log(mylist.tail());
-console.log(mylist.at(4));
-console.log(mylist.at(20));
-console.log(mylist.at(0));
+// console.log(mylist.size());
+// console.log(mylist.head());
+// console.log(mylist.tail());
+// console.log(mylist.at(4));
+// console.log(mylist.at(20));
+// console.log(mylist.at(0));
